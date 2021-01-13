73a2da00325ed10f764e8232618da22c8fa9d1f9
Allen1012 <921543658@qq.com>
2021-01-13 23:27:42 +0800

    114. 二叉树展开为链表
    给定一个二叉树，原地将它展开为一个单链表。

--- src/FlattenBinaryTreeToLinkedList.java
+++ src/FlattenBinaryTreeToLinkedList.java
@@ -2,7 +2,7 @@
  * 114. Flatten Binary Tree to Linked List
  */
 public class FlattenBinaryTreeToLinkedList {
-
+    
     /**
      * Definition for a binary tree node.
      */
@@ -19,8 +19,29 @@ public class FlattenBinaryTreeToLinkedList {
           }
       }
 
-
     public void flatten(TreeNode root) {
+         // 空树直接返回
+        if(root == null){
+            return;
+        }
+        //左节点不为空 将左节点移植右节点 将右节点加入树中
+        if(root.left != null){
+            TreeNode temp = root.right;
+            root.right = root.left;
+            root.left = null;
+            addToTree(root.right,temp);
+        }
+        flatten(root.right);
+    }
+
+    //将结点加至root的第一个空最右节点
+    public void addToTree(TreeNode root,TreeNode temp){
+
+         if(root.right == null){
+             root.right = temp;
+         }else{
+             addToTree(root.right,temp);
+         }
 
     }
 
