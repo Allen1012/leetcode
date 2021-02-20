1742ee303302792cf58e0d281223c89da4f46032
Allen1012 <921543658@qq.com>
2021-02-20 22:02:34 +0800

    1004. 最大连续1的个数 III 添加 官方解法

--- src/MaxConsecutiveOnesIII.java
+++ src/MaxConsecutiveOnesIII.java
@@ -101,4 +101,20 @@ public class MaxConsecutiveOnesIII {
         System.out.println("max:"+max);
         return max;
     }
+
+//官方解法 时间复杂度 O(n)
+//    public int longestOnes(int[] A, int K) {
+//        int n = A.length;
+//        int left = 0, lsum = 0, rsum = 0;
+//        int ans = 0;
+//        for (int right = 0; right < n; ++right) {
+//            rsum += 1 - A[right];
+//            while (lsum < rsum - K) {
+//                lsum += 1 - A[left];
+//                ++left;
+//            }
+//            ans = Math.max(ans, right - left + 1);
+//        }
+//        return ans;
+//    }
 }
